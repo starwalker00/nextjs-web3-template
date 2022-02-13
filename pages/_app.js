613424c7952d9d50ca43fa449ec17b4cce5405c9
starwalker00 <aaronwalkerup@gmail.com>
2022-02-13 14:36:48 +0100

    add global styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import { AppWrapper } from '../state';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, CSSReset, ColorModeProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        color: '#00501e',
+        background: '#e5ffbd',
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AppWrapper>
         <Component {...pageProps} />
       </AppWrapper>
