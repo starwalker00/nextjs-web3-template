d84c3871ce65595ecec63fa5fb73cde78d2de253
starwalker00 <aaronwalkerup@gmail.com>
2022-02-13 15:11:28 +0100

    pretty print json on profile modal

--- components/ProfileItemJSONModal.js
+++ components/ProfileItemJSONModal.js
@@ -1,5 +1,6 @@
-import { Box, Center, Flex, Image, Text, WrapItem, VStack, Spacer, Button, Lorem } from '@chakra-ui/react'
+import { Box, Center, Flex, Image, Text, WrapItem, VStack, Spacer, Button, Link } from '@chakra-ui/react'
 import { useDisclosure } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import {
     Modal,
     ModalOverlay,
@@ -9,25 +10,29 @@ import {
     ModalBody,
     ModalCloseButton,
 } from '@chakra-ui/react'
+import JSONPretty from 'react-json-pretty';
 
 function ProfileItemJSONModal({ profile }) {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const LensHubMumbaiContractURL = "https://mumbai.polygonscan.com/address/0xF6BF84E5df229029C9D36dC7ABaCDBE9c0bd7b4F";
     return (
         <>
             <Button onClick={onOpen}>Open Modal</Button>
 
-            <Modal size='full' isOpen={isOpen} onClose={onClose}>
+            <Modal size='xl' isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>getProfile() contract response</ModalHeader>
-                    <ModalCloseButton />
+                    <ModalHeader>
+                        getProfile() contract response
+                    </ModalHeader>
+                    <ModalCloseButton colorScheme='teal' variant='solid' />
                     <ModalBody>
-                        <Text >
-                            {JSON.stringify(profile)}
+                        <Text overflow='auto'>
+                            <JSONPretty id="json-pretty" data={JSON.stringify(profile)}></JSONPretty>
                         </Text>
                     </ModalBody>
                     <ModalFooter>
-                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                        <Button colorScheme='teal' variant='solid' mr={3} onClick={onClose}>
                             Close
                         </Button>
                     </ModalFooter>
