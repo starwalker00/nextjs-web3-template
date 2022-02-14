fcdca9379b3370a9f8c8e59a2c6a0ce15fde8317
starwalker00 <aaronwalkerup@gmail.com>
2022-02-14 17:35:19 +0100

    support profileId search without UI

--- components/ProfileList.js
+++ components/ProfileList.js
@@ -4,54 +4,64 @@ import { Box, Button, ButtonGroup, Container, Wrap, Center } from '@chakra-ui/re
 import ProfileItem from '../components/ProfileItem'
 import { Spinner } from '@chakra-ui/react'
 import { ethers } from 'ethers';
+import { useState } from 'react';
+
+const PAGE_SIZE = 9
+
+const getKey = (pageIndex, previousPageData, searchValue, pageSize) => {
+    console.log(`pageIndex  : ${JSON.stringify(pageIndex)}`)
+    // pageIndex is not incremented properly on first call, idk why, 2 clicks needed on `load more` for 2nd page
+
+    let cursor // is the first profileId to be newly fetched
+    if (searchValue) { // user searched for a profileId
+        cursor = searchValue
+    }
+    else if (previousPageData) { // user clicked on `load more`
+        let lastDisplayedProfile = previousPageData.slice(-1)
+        let lastDisplayedProfileId = lastDisplayedProfile[0]
+        cursor = lastDisplayedProfileId[0] - 1 // first profileId of the next page
+    } else { // initial loading or reset
+        cursor = null
+    }
+    console.log(`cursor  : ${JSON.stringify(cursor)}`)
+    return [cursor, PAGE_SIZE]
+    // return searchValue
+}
 
 export default function ProfileList({ fallbackData }) {
     // console.log(`fallbackData  : ${JSON.stringify(fallbackData)}`)
+    const [searchValue, setSearchValue] = useState(null)
     const { lensHub } = useAppContext();
-    const profilePerCall = 9;
-    const fetcher = async (cursor) => {
+
+    const fetcher = async (cursor, pageSize) => {
         console.log("fetcher profile");
-        // console.log(`cursor  : ${JSON.stringify(cursor)}`)
-        let profileCount = await lensHub.totalSupply();
-        if (cursor == 999999) { cursor = profileCount.toNumber() };
-        // console.log(`cursor  : ${JSON.stringify(cursor)}`)
-        // console.log(`profileCount  : ${JSON.stringify(profileCount)}`)
-        let profile;
-        let profiles = [];
-        for (let profileId = cursor; profileId > cursor - profilePerCall; profileId--) {
-            if (profileId > 0) {
-                // console.log(`profileId  : ${JSON.stringify(profileId)}`)
-                profile = await lensHub.getProfile(profileId);
-                profile = [profileId, ...profile]
-                if (ethers.BigNumber.isBigNumber(profile[1])) {
-                    // data coming from fallback is already a JS number because it was converted 
-                    // to be able to be serialized when passing props from index.js->getStaticProps
-                    profile[1] = profile[1].toNumber();
-                }
-                profiles.push(profile);
-                // console.log(`profiles  : ${JSON.stringify(profiles)}`)
-            } else {
-                break;
+        console.log(`cursor  : ${JSON.stringify(cursor)}`)
+        let profileTotalSupply = await lensHub.totalSupply();
+        if (!cursor) { // set cursor value to max profileId if not provided
+            cursor = profileTotalSupply.toNumber()
+        }
+        // main profile fetching loop
+        let profile
+        let profiles = []
+        for (let profileId = cursor; (profileId > cursor - pageSize) && profileId > 0; profileId--) {
+            // console.log(`profileId  : ${JSON.stringify(profileId)}`)
+            profile = await lensHub.getProfile(profileId);
+            profile = [profileId, ...profile]
+            if (ethers.BigNumber.isBigNumber(profile[1])) {
+                // data coming from fallback is already a JS number because it was converted 
+                // to be able to be serialized when passing props from index.js->getStaticProps
+                profile[1] = profile[1].toNumber();
             }
+            profiles.push(profile);
+            // console.log(`profiles  : ${JSON.stringify(profiles)}`)
         }
         // console.log(`profiles  : ${JSON.stringify(profiles)}`)
         return profiles
     }
-    const getKey = (pageIndex, previousPageData) => {
-        if (pageIndex === 0) return 999999 // is then transformed into totalNumber of profiles
-        if (previousPageData) { // send last profileId as cursor
-            let tmp = previousPageData.slice(-1);
-            let tmp2 = tmp[0];
-            return tmp2[0] - 1
-        }
-        return previousPageData
-    }
+
     const { data, error, mutate, size, setSize, isValidating } = useSWRInfinite(
-        getKey,
+        (...args) => getKey(...args, searchValue, PAGE_SIZE),
         fetcher,
-        // {
-        //     fallbackData: [[[424, 4, "0x0000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000", "teaismyname", "https://ipfs.io/ipfs/bafkreielgktcju7d5fhrp2qjyz42eceub36jieu6763krit4typwloyzvm", "https://ipfs.io/ipfs/bafkreifqovlwobdqouswjjqfop6hxterpxvyykaxcjkybcsc3q7n4uw5ua"], [41, 3, "0x0000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000", "tiltedsquare", "https://ipfs.io/ipfs/bafkreifkmgarpjlu2yyjcdetakuext4nogmk72j4grkm2o6b4jnxsmtwou", "https://ipfs.io/ipfs/bafkreigkkm5mciwfbz7q25frwglpyqu2onmdxhyyjtureyjvzqkcobosje"]]]
-        // }
         {
             fallbackData: fallbackData,
             revalidateOnMount: true
@@ -78,6 +88,15 @@ export default function ProfileList({ fallbackData }) {
     return (
         <>
             {/* <Box onLoad={() => setSize(size + 1)} d='none'></Box> */}
+            <Button m='5'
+                onClick={() => {
+                    setSearchValue(26);
+                    setSize(1)
+                }}
+                colorScheme='teal' variant='solid'
+            >
+                SET
+            </Button>
             <Wrap spacing='5px' align='center' justify='center'>
                 {results &&
                     results.map(
@@ -91,13 +110,19 @@ export default function ProfileList({ fallbackData }) {
             </Wrap>
             <Center m='10'>
                 <Button m='5'
-                    onClick={() => setSize(1)}
+                    onClick={() => {
+                        setSearchValue(null)
+                        setSize(1)
+                    }}
                     colorScheme='teal' variant='solid'
                 >
                     Reset
                 </Button>
                 <Button m='5'
-                    onClick={() => setSize(size + 1)}
+                    onClick={() => {
+                        setSearchValue(null)
+                        setSize(size + 1)
+                    }}
                     colorScheme='teal' variant='solid'
                     isLoading={isValidating}
                     loadingText='Loading' spinnerPlacement='start'
